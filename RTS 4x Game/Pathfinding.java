1a7eaac012fadff34122a0735ff543effd2b15e8
InfernoPulse <vilhelm3865@gmail.com>
2021-01-11 23:51:22 +0000

    fixed a bug where the adjacency matrix would assume red cells were illegal and implemented binary search so that you can find nodes when performing a star

--- RTS 4x Game/Pathfinding.java
+++ RTS 4x Game/Pathfinding.java
@@ -6,13 +6,19 @@ import java.awt.event.*;
 import java.util.LinkedList;
 
 /**TO DO
- * change 2d buttons array to a normal array
+ * test the binary search
  * add attributes to NodeButton class so we can perform a* on it
  * add astar algorithm
+ * 
+ * to implement a star
+ * need to get every neighbour for a given cell
+ *      use adjacency matrix to find neighbouring cells with an edge
+ *      need to get scores for the neightbours
+ * need to have the score of every neighbour
  */
 
 public class Pathfinding {
-    final int gridSize = 3;
+    final int gridSize = 10;
     final int frameSize = 700;
 
     final Color defColor = Color.BLACK;
@@ -35,13 +41,14 @@ public class Pathfinding {
         JMenuItem search = new JMenuItem("Search");
         JMenuItem clear = new JMenuItem("Clear");
         JMenuItem fill = new JMenuItem("Fill");
+        JMenuItem debug = new JMenuItem("Debug");
 
         search.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 constuctAdjacenyMatrix(buttons);
                 findPOI(buttons);
                 outputPOI();
-                //outputAdjacencyMatrix();
+                outputAdjacencyMatrix();
             }
         });
 
@@ -65,9 +72,23 @@ public class Pathfinding {
             }
         });
 
+        debug.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                int nodeToSearch = 0;
+                NodeButton button = binarySearch(buttons, nodeToSearch, gridSize, 0, gridSize, 0, true);
+                if(button == null){
+                    System.err.println(false);
+                }
+                else{
+                    System.err.println(true);
+                }
+            }
+        });
+
         menu.add(search);
         menu.add(clear);
         menu.add(fill);
+        menu.add(debug);
 
         //iterator used for the nodeID 
         int iterator = 0;
@@ -75,7 +96,7 @@ public class Pathfinding {
             for (int j = 0; j < gridSize; j++) {
                 //adding buttons and setting the background for each button
                 buttons[i][j] = new NodeButton(iterator);
-                buttons[i][j].setBackground(Color.BLACK);
+                buttons[i][j].setBackground(defColor);
                 //if you click on a button it will alternate between black and the colour for that mouse click
                 buttons[i][j].addMouseListener(new MouseInputAdapter(){
 
@@ -120,7 +141,7 @@ public class Pathfinding {
         for (int i = 0; i < gridSize; i++) {
             for (int j = i; j < gridSize; j++) {
                 //if we are currently on a legal node
-                if(buttons[i][j].getBackground() == Color.BLACK){
+                if(buttons[i][j].getBackground() != leftColor){
                     //initialising variables to be used
                     int[] rowChecks = {-1, 1}; //check row positions j - 1, j + 1
                     int[] colChecks = {-1, 1}; //check grid positions i - 1, i + 1
@@ -148,12 +169,12 @@ public class Pathfinding {
                     for (int k = 0; k < 2; k++) {
                         //checks if the colour next to the current grid position is black and if it is adds it to the list of edges
                         if(colChecks[k] != 0){
-                            if(buttons[i + colChecks[k]][j].getBackground() == Color.BLACK){
+                            if(buttons[i + colChecks[k]][j].getBackground() != leftColor){
                                 edges.add(buttons[i + colChecks[k]][j].getNodeID());
                             }
                         }
                         if(rowChecks[k] != 0){
-                            if(buttons[i][j + rowChecks[k]].getBackground() == Color.BLACK){
+                            if(buttons[i][j + rowChecks[k]].getBackground() != leftColor){
                                 edges.add(buttons[i][j + rowChecks[k]].getNodeID());
                             }
                         }
@@ -203,6 +224,46 @@ public class Pathfinding {
             System.err.print(point + " ");
         }
         System.err.println("\n");
+    }
+
+    public NodeButton binarySearch(NodeButton[][] buttons, int nodeID, int outUpper, int outLower, int inUpper, int inLower, boolean inOuter){
+        int outMid = (outUpper + outLower) / 2;
+        if(inOuter){
+            int buttonID = buttons[outMid][0].getNodeID();
+
+            //if the current button is of the same unit as the nodeID
+            if(Math.floorDiv(buttonID, gridSize) == Math.floorDiv(nodeID, gridSize)){
+                inOuter =false;
+            }
+            //if the current button is of greater unit than nodeID
+            else if(Math.floorDiv(buttonID, gridSize) > Math.floorDiv(nodeID, gridSize)){
+                outUpper = outMid - 1;
+            }
+            //if the current button is of lower unit than nodeID
+            else{
+                outLower = outMid + 1;
+            }
+        }
+        else{
+            int inMid = (inUpper + inLower) / 2;
+            NodeButton button = buttons[outMid][inMid];
+
+            if(button.getNodeID() == nodeID){
+                return button;
+            }
+            else if(button.getNodeID() > nodeID){
+                inUpper = inMid - 1;
+            }
+            else{
+                inLower = inMid + 1;
+            }
+        }
+
+        //if there are no more buttons to search or if the unit of the nodeID exceeds that of all buttons then return null
+        
+
+        return binarySearch(buttons, nodeID, outUpper, outLower, inUpper, inLower, inOuter);
+
     }
     
     public static void main(String[] args) {
